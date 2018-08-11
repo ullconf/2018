0017c834bd179a378458eb02de0136556eb7095b
Paul Campbell <paul@rslw.com>
2018-08-11 13:04:33 +0100

    Initial commit
    A basic setup using Nuxt with an Úll logo and a pronunciation.

---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: "ull-2018",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Úll 2018 website" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "https://use.typekit.net/ulo6zhx.css" }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: "#3B8070" },
+  /*
+  ** Build configuration
+  */
+  css: [{ src: "~/assets/stylesheets/main.scss", lang: "scss" }],
+  modules: [["bootstrap-vue/nuxt", { css: false }]],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
+};
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        Teach Úll
+      </h1>
+      <p>
+        /ʧɒk/ <span class="text-danger">ool</span>
+      </p>
+      <div class="links">
+        <a
+          class="btn btn-success"
+          href="https://nuxtjs.org/"
+          target="_blank">Get Tickets</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from "~/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-weight: 500;
+  font-size: 2rem;
+  margin-top: 1rem;
+}
+
+.subtitle {
+  font-weight: 500;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
